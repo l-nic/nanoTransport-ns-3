67969e5f9e20da5276ac4e87425e21fabd912b2e
Serhat Arslan <sarslan@stanford.edu>
2021-01-06 10:12:15 -0800

    Add original raw data from the Homa paper.

--- outputs/homa-paper-reproduction/run-parallel-sim.sh
+++ outputs/homa-paper-reproduction/run-parallel-sim.sh
@@ -3,7 +3,7 @@ for load in 0.8 0.5
 do
     for simIdx in $(seq 0 $(($1-1))) 
     do 
-        nice ./waf --run "scratch/homa-paper-reproduction --duration=0.3 --load=$load --simIdx=$simIdx" &
+        nice ./waf --run "scratch/homa-paper-reproduction --duration=0.5 --load=$load --simIdx=$simIdx" &
         sleep 10
     done
     wait
