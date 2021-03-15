64d03d013a1c3d99120a1ea7d34023c8b55fb197
Serhat Arslan <sarslan@stanford.edu>
2021-03-15 09:09:33 -0700

    Homa and Hpcc figures obtained for NanoPuArcht

--- outputs/homa-paper-reproduction/nanopu-impl/run-parallel-sim.sh
+++ outputs/homa-paper-reproduction/nanopu-impl/run-parallel-sim.sh
@@ -3,7 +3,7 @@ for load in 0.8 0.5
 do
     for simIdx in $(seq 0 $(($1-1))) 
     do 
-        nice ./waf --run "scratch/homa-nanopu-paper-reproduction --load=$load --simIdx=$simIdx --duration=$2 --disableRtx" &
+        nice ./waf --run "scratch/homa-nanopu-paper-reproduction --load=$load --simIdx=$simIdx --duration=$2 --disableRtx --bdpPkts=$3" &
         sleep 10
     done
     wait
