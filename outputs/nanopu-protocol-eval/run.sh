25db1af61c52a985618aa27e64ec31d774af785c
Serhat Arslan <sarslan@stanford.edu>
2021-04-06 13:19:59 -0700

    NanoPuArcht protocol evaluation experiments repeated with different configurations.

--- outputs/nanopu-protocol-eval/run.sh
+++ outputs/nanopu-protocol-eval/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # for buffSize in 74 37
-for buffSize in 91 53
+# for buffSize in 91 53
+for buffSize in 110 58
 do
     for protocol in "ndp" "homa" "homatr" 
     do 
-        nice ./waf --run "scratch/nanopu-protocol-eval --protocol=$protocol --bufferSize=$buffSize --delay=88ns --initialCredit=10 --rto=6.0 --minMsgSize=15 --mtu=1088"
+        nice ./waf --run "scratch/nanopu-protocol-eval --protocol=$protocol --bufferSize=$buffSize --delay=131ns --initialCredit=12 --rto=6.0 --minMsgSize=18 --mtu=1088"
     done
 done
